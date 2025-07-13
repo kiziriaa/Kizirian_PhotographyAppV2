7de975ad800d8e8a52bb5765da3f28395f0b5270
Alex <kiziriaa@oregonstate.edu>
2025-07-13 14:58:09 -0500

    initial commit - Kizirian Photography Site

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Kizirian Photography</Link>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/gallery">Gallery</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/booking">Booking</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
